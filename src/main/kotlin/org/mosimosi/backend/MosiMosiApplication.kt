ffb9d3b202097f1e8a7e080cd435c26f9c0a896c
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-12-16 14:48:59 +0900

    init

---
+++ src/main/kotlin/org/mosimosi/backend/MosiMosiApplication.kt
@@ -0,0 +1,11 @@
+package org.mosimosi.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MosiMosiApplication
+
+fun main(args: Array<String>) {
+	runApplication<MosiMosiApplication>(*args)
+}
---
+++ src/test/kotlin/org/mosimosi/backend/MosiMosiApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.mosimosi.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MosiMosiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
